49a982b47a667b951f267cdd1a229a97b5acd8da
Pandax19 <ajwhitley@comcast.net>
2023-04-13 00:07:36 -0400

    added user score and initials to page and edited css

--- assets/script/script.js
+++ assets/script/script.js
@@ -56,7 +56,7 @@ let questions= [
 let timerId;
 let timeLeft= 150;
 let currentIndex= 0;
-
+let playAgainDiv=document.getElementById("playagain")
 let rightWrong = document.getElementById("rightWrong");
 let startButton= document.getElementById("start-btn");
 let timeEl= document.getElementById("time");
@@ -72,6 +72,8 @@ let player = {
     "initials" : userInitials, 
     "score" : playerScore,
 };
+
+
 
 let userScores = localStorage.setItem("scores", JSON.stringify(player));
 let allPlayerScores = [];
@@ -98,11 +100,14 @@ function startQuiz(){
 function endQuiz(){
     player.initials = prompt("Please enter your initials");
     player.score = timeLeft;
+    
     localStorage.setItem("scores", JSON.stringify(player));
-    allPlayerScores.push(player)
+    // allPlayerScores.push(player);
+
     // let userScores = localStorage.setItem(userInitials, playerScore);
     // console.log(player)
-    // renderLastPlayer();
+    renderLastPlayer();
+    // playAgain();
     // return userScores;
 };
 
@@ -164,15 +169,34 @@ function timerFunc(){
 }
 
 function renderLastPlayer() {
-    player = localStorage.getItem("player", JSON.parse(playerScore));
+    let player = JSON.parse(localStorage.getItem("scores"));
+    let lastUserInitials = player.initials;
+    let lastUserScore = player.score;
+    let initialDiv= document.createElement("h3");
+    let scoreDiv = document.createElement("h3");
+    initialDiv.setAttribute("class", "highscore");
+    scoreDiv.setAttribute("class", "highscore");
+    initialDiv.textContent= `User Initials: ${lastUserInitials}`;
+    scoreDiv.textContent= `High Score: ${lastUserScore}`;
+    highscoresDiv.appendChild(initialDiv);
+    highscoresDiv.appendChild(scoreDiv);
+    // playAgain();
     // console.log(lastPlayer);
     // let savedPlayer= JSON.parse(localStorage.getItem(savedPlayer));
     // console.log(savedPlayer);
-
     // highscoresDiv.textContent = playerId + " : " + lastUserScore;
-
 }
 
-function renderHighScores() {
+// function playAgain(){
+//     let playAgainButton = document.createElement("Button");
+//     playAgainButton.setAttribute("class", "playagainbutton");
+//     playAgainButton.textContent="Play Again?";
+//     playAgainDiv.appendChild(playAgainButton);
+    
+//     // timeLeft= 150;
+//     // currentIndex= 0;
 
-}
+//     playAgainButton.addEventListener("click", startQuiz());
+
+
+// }
